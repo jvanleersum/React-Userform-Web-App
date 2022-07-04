aa33d6e739c805b027d7d16a69b577d31db2e7ae
Judith van Leersum <judithvleersum@gmail.com>
2022-07-04 13:56:46 +0200

    Added portals for modal

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,24 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import Button from "./Button";
 import Card from "./Card";
-import classes from './Modal.module.css';
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} onClick={props.onDismiss}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={classes.modal}>
+        <header className={classes.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={classes.content}>
+          <p>{props.content}</p>
+        </div>
+        <footer className={classes.actions}>
+          <Button type="button" onClick={props.onDismiss}>
+            Okay
+          </Button>
+        </footer>
+      </Card>
+  );
+} 
 
 const Modal = (props) => {
-
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onDismiss}></div>
-    <Card className={classes.modal}>
-      <header className={classes.header}>
-        <h2>{props.title}</h2>
-      </header>
-      <div className={classes.content}>
-        <p>{props.content}</p>
-      </div>
-      <footer className={classes.actions}>
-        <Button type="button" onClick={props.onDismiss}>Okay</Button>
-      </footer>
-    </Card>
-    </div>
+    <React.Fragment>
+      { ReactDOM.createPortal(<Backdrop onDismiss={props.onDismiss} />, document.getElementById('backdrop-root')) }
+      { ReactDOM.createPortal(<ModalOverlay title={props.title} content={props.content} onDismiss={props.onDismiss}/>, document.getElementById('overlay-root'))}
+    </React.Fragment>
   );
 };
 
